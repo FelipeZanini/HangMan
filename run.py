ba88e8ea4d698b2a3db1d64eedf65da09c2a899e
Felipe Silva <felipe_zanini@hotmail.com>
2023-07-02 23:19:52 +0000

    Remove the trailing whitespace in the file

--- run.py
+++ run.py
@@ -14,7 +14,7 @@ def get_word():
 def play(word, guessed):
     """
     Responsible for getting the guesses from the user, and assures that
-    only single letters or the whole word will be inputted, calling the 
+    only single letters or the whole word will be inputted, calling the
     function to display the hangman whether the player failed in his attempt
     or guessed the letter or the mystery word.
     """
