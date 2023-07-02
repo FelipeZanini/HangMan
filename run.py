31efa4b8b766007e7ffeda0f9ae606674e7ca6e5
Felipe Silva <felipe_zanini@hotmail.com>
2023-07-02 22:48:12 +0000

    Add the get_word function, that gets a random word, from the list of words

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
 import random
 from words import WORDS_LIST
 from hangman import HANGMAN_STAGES
+
+
+def get_word():
+    """
+    Get a random word from a list of words.
+    """
+    word = random.choice(WORDS_LIST).upper()
+    return word
